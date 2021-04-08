01eaeb9c9e4371baad37f1dbd3aca7dcd8aa075d
jarvis-001 <61859855+jarvis-001@users.noreply.github.com>
2021-04-08 20:07:35 +0530

    Update Cards.jsx

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -14,7 +14,7 @@ const Cards = ({data:{confirmed, recovered, deaths, lastUpdate}}) =>
                 <Grid item component= {Card} xs={12} md={3} className={cx(styles.card,styles.infected)}>
                    <CardContent>
                        <Typography color="textSecondary" gutterBottom>Infected</Typography>
-                       <Typography variant ="h5"><CountUp start ={0} end = {confirmed.value} duration ={2.5} separator = ','/></Typography>
+                       <Typography variant ="h4"><CountUp start ={0} end = {confirmed.value} duration ={2.5} separator = ','/></Typography>
                        <Typography color ="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
                        <Typography variant="body2">no of active cases from Covid-19</Typography>
                    </CardContent> 
@@ -22,7 +22,7 @@ const Cards = ({data:{confirmed, recovered, deaths, lastUpdate}}) =>
                 <Grid item component= {Card} xs={12} md={3} className={cx(styles.card,styles.recovered)}>
                    <CardContent>
                        <Typography color="textSecondary" gutterBottom>Recovered</Typography>
-                       <Typography variant ="h5"><CountUp start ={0} end = {recovered.value} duration ={2.5} separator = ','/></Typography>
+                       <Typography variant ="h4"><CountUp start ={0} end = {recovered.value} duration ={2.5} separator = ','/></Typography>
                        <Typography color ="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
                        <Typography variant="body2">no of recovered cases from Covid-19</Typography>
                    </CardContent> 
@@ -30,7 +30,7 @@ const Cards = ({data:{confirmed, recovered, deaths, lastUpdate}}) =>
                 <Grid item component= {Card} xs={12} md={3} className={cx(styles.card,styles.deaths)}>
                    <CardContent>
                        <Typography color="textSecondary" gutterBottom>Deaths</Typography>
-                       <Typography variant ="h5"><CountUp start ={0} end = {deaths.value} duration ={2.5} separator = ','/></Typography>
+                       <Typography variant ="h4"><CountUp start ={0} end = {deaths.value} duration ={2.5} separator = ','/></Typography>
                        <Typography color ="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
                        <Typography variant="body2">no of deaths from Covid-19</Typography>
                    </CardContent> 
